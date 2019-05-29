a465483756b1dadc0b2ca48b07f1f21e666e8f3c
zhangnanning <1027963459@qq.com>
2019-05-29 10:30:30 +0800

    init

---
+++ src/main/src/web/node/app.js
@@ -0,0 +1,24 @@
+const Koa = require("koa");
+const path = require("path");
+const bodyparser = require("koa-bodyparser");
+const Router = require("koa-router");
+const koaStatic = require("koa-static");
+
+const app = new Koa();
+const router = new Router();
+
+// const staticPath = "./webapp";
+const staticPath = "/opt/www/gitbook/_book";
+
+app.use(bodyparser());
+
+app.use(router.routes()).
+    use(router.allowedMethods());
+
+app.use(koaStatic(path.join(__dirname, staticPath)));
+
+app.listen(3000, () => {
+
+    console.log("[Koa2] start-quick is starting at port 3000");
+
+});
